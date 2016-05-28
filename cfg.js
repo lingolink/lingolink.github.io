ccc7c571d88e2c7915de6399a1a654a39dfc3e0c
ducn <ngoduc.smpt@gmail.com>
2016-05-28 09:01:52 +0700

    aa

--- cfg.js
+++ cfg.js
@@ -1,1 +1,10 @@
-{'clip':{'url':'http://stream.flowplayer.org/Extremists.flv','pageUrl':'lingolink.github.io'}}
+{
+  'clip': {
+    'pageUrl': 'lingolink.github.io',
+     // when clip starts, this function is called
+     onStart: function(clip) {
+	      var url = "https://dl.dropboxusercontent.com/s/32zjrv20u76il80/NatureIsSpeakingMotherNature-01.mp4?dl";
+     	  $f().resume().play(url);
+     }
+  }
+}
