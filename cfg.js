605a12402ead9857c5850fd8a1cfeaddd27c13e1
ducn <ngoduc.smpt@gmail.com>
2016-05-28 09:13:47 +0700

    aa

--- cfg.js
+++ cfg.js
@@ -1,10 +1,1 @@
-{
-  'clip': {
-    'pageUrl': 'lingolink.github.io',
-     // when clip starts, this function is called
-     onStart: function(clip) {
-	      var url = "https://dl.dropboxusercontent.com/s/32zjrv20u76il80/NatureIsSpeakingMotherNature-01.mp4?dl";
-     	  $f().resume().play(url);
-     }
-  }
-}
+{'clip':{'pageUrl':'lingolink.github.io',autoPlay:false,onStart:function(clip){alert("AA");$f().play("http://stream.flowplayer.org/Extremists.flv");}}}
